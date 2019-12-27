7efa82844a58358f1229b8698552f5c1e02d3a4a
Andreas Huber <andrehub@ifi.uio.no>
2019-12-27 14:21:17 +0100

    Buffered input stream

--- src/main/scala/ReadFile.scala
+++ src/main/scala/ReadFile.scala
@@ -1,15 +1,28 @@
-import java.io.{File, FileInputStream}
+import java.io.{BufferedInputStream, File, FileInputStream}
+
 import com.github.luben.zstd.ZstdInputStream
+
 import scala.language.postfixOps
 import scala.language.reflectiveCalls
 import Control._
 
 object ReadFile extends App {
-  val filename = "C:\\_\\ztest\\test.txt.zst"
+  val filename = "C:\\_\\ztest\\Reddit_Subreddits.ndjson.zst"
+  // val filename = "C:\\_\\ztest\\test.txt.zst"
 
-  using(new FileInputStream(new File(filename))) { fileInputStream =>{
-    using(new ZstdInputStream(fileInputStream)) { in => {
-      scala.io.Source.fromInputStream(in).getLines().foreach(println)
+
+  val startedAtNanos = System.nanoTime()
+
+
+  using(new FileInputStream(new File(filename))) { fileInputStream => {
+    using(new BufferedInputStream(fileInputStream)) { bufferedInputStream => {
+      using(new ZstdInputStream(fileInputStream)) { zstdInputStream => {
+        var count = 0;
+        val result = scala.io.Source.fromInputStream(zstdInputStream).getLines().count(a => true);
+        println("Lines: " + result)
+      }}
     }}
   }}
+
+  println("Milliseconds: " + (System.nanoTime() - startedAtNanos) / 1_000_000)
 }
