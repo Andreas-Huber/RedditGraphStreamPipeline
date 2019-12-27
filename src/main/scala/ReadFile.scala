3df41eba5111ba62d9b943e2bcd8395db3ecd947
Andreas Huber <andrehub@ifi.uio.no>
2019-12-27 22:20:53 +0100

    Add benchmark logs

--- src/main/scala/ReadFile.scala
+++ src/main/scala/ReadFile.scala
@@ -7,7 +7,7 @@ import scala.language.reflectiveCalls
 import Control._
 
 object ReadFile extends App {
-  val filename = "C:\\_\\ztest\\Reddit_Subreddits.ndjson.zst"
+  val filename = "//media//andreas//Andal//reddit//Reddit_Subreddits.ndjson.zst"
   // val filename = "C:\\_\\ztest\\test.txt.zst"
 
 
@@ -16,13 +16,37 @@ object ReadFile extends App {
 
   using(new FileInputStream(new File(filename))) { fileInputStream => {
     using(new BufferedInputStream(fileInputStream)) { bufferedInputStream => {
-      using(new ZstdInputStream(fileInputStream)) { zstdInputStream => {
+      using(new ZstdInputStream(bufferedInputStream)) { zstdInputStream => {
         var count = 0;
+
+        while(zstdInputStream.transferTo())
+
         val result = scala.io.Source.fromInputStream(zstdInputStream).getLines().count(a => true);
         println("Lines: " + result)
       }}
     }}
   }}
 
-  println("Milliseconds: " + (System.nanoTime() - startedAtNanos) / 1_000_000)
+  println("Seconds: " + (System.nanoTime() - startedAtNanos) / 1_000_000_000)
 }
+
+
+/*
+time zstd -t
+
+Reddit_Subreddits.ndjson.zst: 78688602372 bytes
+
+real	0m31,697s
+user	0m30,284s
+sys	0m1,412s
+
+---------------------------
+Input stream
+Lines: 49723295
+Seconds: 183
+
+
+---------------------------
+Buffered input stream
+Seconds: 185
+ */
