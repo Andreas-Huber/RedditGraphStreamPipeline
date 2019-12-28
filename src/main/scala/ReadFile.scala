ecd6c650784ab714414a83dfc838f2c2f8508642
Andreas Huber <andrehub@ifi.uio.no>
2019-12-28 20:29:50 +0100

    Read stream without line split

--- src/main/scala/ReadFile.scala
+++ src/main/scala/ReadFile.scala
@@ -7,8 +7,8 @@ import scala.language.reflectiveCalls
 import Control._
 
 object ReadFile extends App {
-  val filename = "//media//andreas//Andal//reddit//Reddit_Subreddits.ndjson.zst"
-  // val filename = "C:\\_\\ztest\\test.txt.zst"
+  //val filename = "//media//andreas//Andal//reddit//Reddit_Subreddits.ndjson.zst"
+  val filename = "C:\\_\\ztest\\Reddit_Subreddits.ndjson.zst"
 
 
   val startedAtNanos = System.nanoTime()
@@ -16,13 +16,20 @@ object ReadFile extends App {
 
   using(new FileInputStream(new File(filename))) { fileInputStream => {
     using(new BufferedInputStream(fileInputStream)) { bufferedInputStream => {
-      using(new ZstdInputStream(bufferedInputStream)) { zstdInputStream => {
-        var count = 0;
+      using(new ZstdInputStream(fileInputStream)) { zstdInputStream => {
 
-        while(zstdInputStream.transferTo())
 
-        val result = scala.io.Source.fromInputStream(zstdInputStream).getLines().count(a => true);
-        println("Lines: " + result)
+        var a: Int = 0;
+        while({
+          a = zstdInputStream.available()
+          a > 0}) {
+          zstdInputStream.readNBytes(a);
+        }
+
+
+//        var count = 0;
+//        val result = scala.io.Source.fromInputStream(zstdInputStream).getLines().count(a => true);
+//        println("Lines: " + result)
       }}
     }}
   }}
@@ -32,21 +39,36 @@ object ReadFile extends App {
 
 
 /*
-time zstd -t
+time zstd -t (linux ubuntu)
 
 Reddit_Subreddits.ndjson.zst: 78688602372 bytes
 
 real	0m31,697s
 user	0m30,284s
 sys	0m1,412s
-
 ---------------------------
-Input stream
+time zstd -t (WSL ubuntu)
+Reddit_Subreddits.ndjson.zst: 78688602372 bytes
+
+real    0m46.030s
+user    0m33.578s
+sys     0m12.391s
+---------------------------
+Input stream linux
 Lines: 49723295
 Seconds: 183
 
+---------------------------
+Buffered input stream linux
+Seconds: 185
 
 ---------------------------
-Buffered input stream
-Seconds: 185
+Input stream windows
+Seconds: 191
+
+
+
+---------------------------
+Buffered Input stream windows
+Seconds: 46
  */
