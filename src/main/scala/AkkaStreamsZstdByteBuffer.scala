a97593ccbf6e472aca36c908b90968a4d4ec2aa8
Andreas Huber <andrehub@ifi.uio.no>
2020-06-30 12:32:24 +0200

    Byte buffer zstd not working

--- src/main/scala/AkkaStreamsZstdByteBuffer.scala
+++ src/main/scala/AkkaStreamsZstdByteBuffer.scala
@@ -1,17 +1,20 @@
 import java.io.{File, FileOutputStream}
+import java.nio.ByteBuffer
 import java.nio.file.Paths
 
 import AkkaStreamsRead.{eventualResult, fileout, outputStream, system}
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.IOResult
-import akka.stream.scaladsl.{FileIO, Sink, Source, StreamConverters}
+import akka.stream.scaladsl.{FileIO, Flow, Sink, Source, StreamConverters}
 import akka.util.ByteString
+import com.github.luben.zstd.Zstd
 
 import scala.concurrent.Future
 
 object AkkaStreamsZstdByteBuffer extends App{
   implicit val system = ActorSystem("ByteBufferTest")
-  val filename = "G:\\temp\\RA_78M.csv.ultra.gz"
+  val filename = "G:\\temp\\RA.sample.csv"
   val fileout = "D:\\temp\\out.bytebuff"
 
   val startTime = System.nanoTime
@@ -20,10 +23,16 @@ object AkkaStreamsZstdByteBuffer extends App{
 
   val source: Source[ByteString, Future[IOResult]] = FileIO.fromPath(Paths.get(filename))
 
-  val sink: Sink[ByteString, Future[IOResult]] = StreamConverters.fromOutputStream(() => outputStream)
+  val sink: Sink[ByteString, Future[IOResult]] = FileIO.toPath(Paths.get(fileout))
 
+  val decompressZstd: Flow[ByteString, ByteString, NotUsed] = Flow[ByteString].map(byteString => {
+    val buffer = ByteBuffer.allocate(byteString.length)
+    Zstd.compress(buffer, byteString.asByteBuffer)
+    ByteString.fromByteBuffer(buffer)
+  })
 
   val eventualResult = source
+    .via(decompressZstd)
     .runWith(sink)
 
 
