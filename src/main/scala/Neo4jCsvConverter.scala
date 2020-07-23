212725a7da454fb453d304c5706d7772b26232d2
Andreas Huber <andrehub@ifi.uio.no>
2020-07-23 14:21:22 +0200

    Creaded ndjson to csv subflow

--- src/main/scala/Neo4jCsvConverter.scala
+++ src/main/scala/Neo4jCsvConverter.scala
@@ -1,27 +1,25 @@
 import java.io.{BufferedInputStream, File, FileInputStream, FileNotFoundException}
 import java.nio.charset.StandardCharsets
-import java.nio.file.{Path, Paths}
+import java.nio.file.{Paths}
 
 import akka.actor.ActorSystem
 import akka.stream.IOResult
-import akka.stream.scaladsl.{FileIO, Flow, Framing, JsonFraming, Sink, Source, StreamConverters}
+import akka.stream.scaladsl.{FileIO, Flow, Framing, Sink, Source, StreamConverters}
 import akka.util.ByteString
 import org.apache.commons.compress.compressors.{CompressorException, CompressorStreamFactory}
 
 import scala.concurrent.Future
 import spray.json._
 import DefaultJsonProtocol._
-import akka.NotUsed
-import com.typesafe.config.Optional
 import model.Submission
 
-import scala.collection.immutable
 import akka.stream.alpakka.csv.scaladsl.{CsvFormatting, CsvQuotingStyle}
 
 object Neo4jCsvConverter extends App{
   implicit val system = ActorSystem("ReadArchives")
   val fileIn = "C:\\import\\RS_v2_2008-03.gz"
   val fileOut = "C:\\import\\RS.out"
+  val importPath = Paths.get("C:\\import\\submissions\\")
 
   // create the formats and provide them implicitly
   implicit val submissionFormat = jsonFormat2(Submission)
@@ -33,32 +31,31 @@ object Neo4jCsvConverter extends App{
   val source: Source[ByteString, Future[IOResult]] = getCompressorInputStreamSource(fileIn)
   val sourceUc = FileIO.fromPath(Paths.get("C:\\import\\RS_2015-01"));
 
-  val toCsvLine: Flow[immutable.Seq[String], ByteString, _] = CsvFormatting.format(
-    CsvFormatting.Comma,
-    CsvFormatting.DoubleQuote,
-    CsvFormatting.Backslash,
-    "\n",
-    CsvQuotingStyle.Required,
-    StandardCharsets.UTF_8,
-    None)
-
   val sink: Sink[ByteString, Future[IOResult]] = FileIO.toPath(Paths.get(fileOut))
 
 
-
-  val eventualResult = source
+  // Takes a NdJson ByteStrings and spits them out as CSV ByteStrings
+  val ndJsonToCsvConverter = Flow[ByteString]
     .via(Framing.delimiter( //chunk the inputs up into actual lines of text
       ByteString("\n"),
       maximumFrameLength = Int.MaxValue,
       allowTruncation = true))
-//   .via(JsonFraming.objectScanner(Int.MaxValue))
-
     .map(_.utf8String)
-    .map(_.parseJson.convertTo[Submission].toSeq)
-    .via(toCsvLine)
+    .map(_.parseJson.convertTo[Submission].toSeq) // Create json objects and then sequences of strings
+    .via(CsvFormatting.format( // Create csv line
+      CsvFormatting.Comma,
+      CsvFormatting.DoubleQuote,
+      CsvFormatting.Backslash,
+      "\n",
+      CsvQuotingStyle.Required,
+      StandardCharsets.UTF_8,
+      None))
 
-    //.runForeach(i => print(i.utf8String))
-    .runWith(sink)
+
+  val eventualResult = source
+    .via(ndJsonToCsvConverter)
+    .runForeach(i => print(i.utf8String))
+    //.runWith(sink)
 
 
   implicit val ec = system.dispatcher
