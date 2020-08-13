e68cf8e037ddf35a69e23e21776e46b0080a32b7
Andreas Huber <andreas.huber@infinite-coding.com>
2020-08-13 15:51:03 +0200

    Impemented simple ndJsonToSumbission test

--- src/test/scala/no/simula/umod/redditdatasetstreampipeline/FlowsSpec.scala
+++ src/test/scala/no/simula/umod/redditdatasetstreampipeline/FlowsSpec.scala
@@ -2,6 +2,7 @@ package no.simula.umod.redditdatasetstreampipeline
 
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Sink, Source}
+import akka.util.ByteString
 import no.simula.umod.redditdatasetstreampipeline.model.Submission
 import org.scalactic.source.Position
 import org.scalatest.BeforeAndAfter
@@ -14,19 +15,22 @@ class FlowsSpec extends AnyFlatSpec with BeforeAndAfter {
   implicit val system = ActorSystem("Test")
 
   override protected def after(fun: => Any)(implicit pos: Position): Unit = system.terminate()
-//
-//  "ndJsonToSubmission" should "should convert ToCsv traits to CSV ByteStrings" in {
-//    val conv = Flows.objectToCsv
-//
-//    val result = Source.single(new Submission(Option("SR"), Option("555")))
-//      .via(conv)
-//      .map(_.utf8String)
-//      .runWith(Sink.seq)
-//
-//    val res = Await.result(result, 3.seconds);
-//    assert(res(0) === "SR,555\n");
-//
-//  }
+
+  "ndJsonToSubmission" should "should convert json byte strings to Submission objects" in {
+    val conv = Flows.ndJsonToSubmission
+
+    val expected = Submission(Option("SR"), Option("a"))
+    val json = "{\"id\":\"a\",\"subreddit\":\"SR\"}\n"
+
+
+    val result = Source.single(ByteString(json))
+      .via(conv)
+      .runWith(Sink.seq)
+
+    val res = Await.result(result, 3.seconds);
+    assert(res(0) === expected)
+    assert(res.length === 1)
+  }
 
   "objectToCsv" should "should convert ToCsv traits to CSV ByteStrings" in {
     val conv = Flows.objectToCsv
