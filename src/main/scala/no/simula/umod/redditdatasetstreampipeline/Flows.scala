9d35dd910880e54fe857d39ae2b3e5b780b3b422
Andreas Huber <andreas.huber@infinite-coding.com>
2020-09-22 15:03:37 +0200

    Removed unused flow and converted json test to ndJsonToSubmission test

--- src/main/scala/no/simula/umod/redditdatasetstreampipeline/Flows.scala
+++ src/main/scala/no/simula/umod/redditdatasetstreampipeline/Flows.scala
@@ -3,15 +3,12 @@ package no.simula.umod.redditdatasetstreampipeline
 import java.nio.charset.StandardCharsets
 
 import akka.NotUsed
-import akka.stream.{FlowShape, IOResult}
 import akka.stream.alpakka.csv.scaladsl.{CsvFormatting, CsvQuotingStyle}
-import akka.stream.scaladsl.{Balance, Flow, Framing, GraphDSL, Merge, Source}
+import akka.stream.scaladsl.{Flow, Framing}
 import akka.util.ByteString
 import no.simula.umod.redditdatasetstreampipeline.model.JsonFormats._
 import no.simula.umod.redditdatasetstreampipeline.model.{Submission, ToCsv}
 import spray.json._
-
-import scala.concurrent.Future
 
 object Flows {
 
@@ -41,30 +38,4 @@ object Flows {
       CsvQuotingStyle.Required,
       StandardCharsets.UTF_8,
       None))
-
-  /**
-   * Parallel flat map concat
-   * Source: https://stackoverflow.com/questions/46972340/how-to-do-a-parallel-flatmapconcat-in-akka-streams
-   *
-   * @param parallelism
-   * @param sourceCreator
-   * @tparam I
-   * @tparam O
-   * @return
-   */
-  def parallelFlatMapConcat[I, O](parallelism: Int)(sourceCreator: I => Source[O, Future[IOResult]]) = {
-    val mapConcat = Flow[I].flatMapConcat(sourceCreator)
-
-    GraphDSL.create() { implicit builder =>
-      import GraphDSL.Implicits._
-
-      val balance = builder.add(Balance[I](parallelism))
-      val merge = builder.add(Merge[O](parallelism))
-
-      for (i ← 0 until parallelism)
-        balance.out(i) ~> mapConcat ~> merge.in(i)
-
-      FlowShape(balance.in, merge.out)
-    }
-  }
 }
--- src/test/scala/no/simula/umod/redditdatasetstreampipeline/FlowsSpec.scala
+++ src/test/scala/no/simula/umod/redditdatasetstreampipeline/FlowsSpec.scala
@@ -1,12 +1,9 @@
 package no.simula.umod.redditdatasetstreampipeline
 
-import java.nio.file.Paths
-
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{FileIO, Keep, Sink, Source}
+import akka.stream.scaladsl.{Sink, Source}
 import akka.util.ByteString
-import no.simula.umod.redditdatasetstreampipeline.Neo4jCsvConverter.{fileOut, filesSource, numberOfThreads}
-import no.simula.umod.redditdatasetstreampipeline.model.{Submission, ToCsv}
+import no.simula.umod.redditdatasetstreampipeline.model.Submission
 import org.scalactic.source.Position
 import org.scalatest.BeforeAndAfter
 import org.scalatest.flatspec.AnyFlatSpec
@@ -23,7 +20,7 @@ class FlowsSpec extends AnyFlatSpec with BeforeAndAfter {
     val conv = Flows.ndJsonToSubmission
 
     val expected = Submission(Option("SR"), Option("a"))
-    val json = "{\"id\":\"a\",\"subreddit\":\"SR\"}\n"
+    val json = """{"id":"a","subreddit":"SR"}"""
 
 
     val result = Source.single(ByteString(json))
@@ -33,6 +30,23 @@ class FlowsSpec extends AnyFlatSpec with BeforeAndAfter {
     val res = Await.result(result, 3.seconds);
     assert(res(0) === expected)
     assert(res.length === 1)
+  }
+
+  "ndJsonToSubmission" should "be able to deserialize a line from the reddit submissions dataset" in {
+    val conv = Flows.ndJsonToSubmission
+
+    val expected = Submission(Option("Jeff_Is_Fat"), Option("6dis8"))
+    val json = """{"archived":true,"author":"[deleted]","author_flair_background_color":"","author_flair_css_class":null,"author_flair_text":null,"author_flair_text_color":"dark","brand_safe":false,"can_gild":false,"contest_mode":false,"created_utc":1206577677,"distinguished":null,"domain":"self.reddit.com","edited":false,"gilded":0,"hidden":false,"hide_score":false,"id":"6dis8","is_crosspostable":false,"is_reddit_media_domain":false,"is_self":false,"is_video":false,"link_flair_css_class":null,"link_flair_richtext":[],"link_flair_text":null,"link_flair_text_color":"dark","link_flair_type":"text","locked":false,"media":null,"media_embed":{},"no_follow":true,"num_comments":0,"num_crossposts":0,"over_18":false,"parent_whitelist_status":null,"permalink":"\/r\/Jeff_Is_Fat\/comments\/6dis8\/jeff_is_fat\/","retrieved_on":1522687818,"rte_mode":"markdown","score":1,"secure_media":null,"secure_media_embed":{},"selftext":"[deleted]","send_replies":true,"spoiler":false,"stickied":false,"subreddit":"Jeff_Is_Fat","subreddit_id":"t5_2qhnz","subreddit_name_prefixed":"r\/Jeff_Is_Fat","subreddit_type":"public","suggested_sort":null,"thumbnail":"default","thumbnail_height":null,"thumbnail_width":null,"title":"Jeff is fat!","url":"http:\/\/self.reddit.com","whitelist_status":null}"""
+
+
+    val result = Source.single(ByteString(json))
+      .via(conv)
+      .runWith(Sink.seq)
+
+    val res = Await.result(result, 3.seconds);
+    assert(res(0) === expected)
+    assert(res.length === 1)
+
   }
 
   "objectToCsv" should "should convert ToCsv traits to CSV ByteStrings" in {
@@ -48,9 +62,5 @@ class FlowsSpec extends AnyFlatSpec with BeforeAndAfter {
 
   }
 
-  "jsonParser" should "be able to deserialize lines from the archives" in {
-    import spray.json._
-    val source = """{"archived":true,"author":"[deleted]","author_flair_background_color":"","author_flair_css_class":null,"author_flair_text":null,"author_flair_text_color":"dark","brand_safe":false,"can_gild":false,"contest_mode":false,"created_utc":1206577677,"distinguished":null,"domain":"self.reddit.com","edited":false,"gilded":0,"hidden":false,"hide_score":false,"id":"6dis8","is_crosspostable":false,"is_reddit_media_domain":false,"is_self":false,"is_video":false,"link_flair_css_class":null,"link_flair_richtext":[],"link_flair_text":null,"link_flair_text_color":"dark","link_flair_type":"text","locked":false,"media":null,"media_embed":{},"no_follow":true,"num_comments":0,"num_crossposts":0,"over_18":false,"parent_whitelist_status":null,"permalink":"\/r\/Jeff_Is_Fat\/comments\/6dis8\/jeff_is_fat\/","retrieved_on":1522687818,"rte_mode":"markdown","score":1,"secure_media":null,"secure_media_embed":{},"selftext":"[deleted]","send_replies":true,"spoiler":false,"stickied":false,"subreddit":"Jeff_Is_Fat","subreddit_id":"t5_2qhnz","subreddit_name_prefixed":"r\/Jeff_Is_Fat","subreddit_type":"public","suggested_sort":null,"thumbnail":"default","thumbnail_height":null,"thumbnail_width":null,"title":"Jeff is fat!","url":"http:\/\/self.reddit.com","whitelist_status":null}"""
-    val jsonAst = source.parseJson
-  }
+
 }
