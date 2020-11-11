b6e256971de38c1d0a4982d4673c8c78d5112b4f
Andreas Huber <andreas.huber@infinite-coding.com>
2020-11-11 14:49:33 +0100

    Added symlink to the update script

--- update-reddit-dataset-stream-pipeline.sh
+++ update-reddit-dataset-stream-pipeline.sh
@@ -4,5 +4,15 @@
 # The newest version gets automatically updated by the Github Actions Release Build, as soon
 # as the master branch changes.
 
+cd ~/bin
 rm -r redditdatasetstreampipeline-0.1/
 curl --silent https://redditpipeline.blob.core.windows.net/deploy/rdsp-latest.zip | jar xv
+chmod +x redditdatasetstreampipeline-0.1/bin/redditdatasetstreampipeline
+
+if [ ! -f "rdsp" ]; then
+        # Create softlink to the executable
+        ln -s redditdatasetstreampipeline-0.1/bin/redditdatasetstreampipeline rdsp
+
+fi
+
+echo "Successfully installed. Use the command rdsp to call the program"
