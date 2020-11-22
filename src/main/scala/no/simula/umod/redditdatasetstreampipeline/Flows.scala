c84656e469aba22a1509db2c265b98df3880612b
Andreas Huber <andreas.huber@infinite-coding.com>
2020-11-22 20:32:55 +0100

    Changed the json framing delimiter because of corrupted comments

--- src/main/scala/no/simula/umod/redditdatasetstreampipeline/Flows.scala
+++ src/main/scala/no/simula/umod/redditdatasetstreampipeline/Flows.scala
@@ -6,11 +6,11 @@ import java.nio.charset.StandardCharsets
 import akka.NotUsed
 import akka.stream.IOResult
 import akka.stream.alpakka.csv.scaladsl.{CsvFormatting, CsvQuotingStyle}
-import akka.stream.scaladsl.{Flow, Framing, Source, StreamConverters}
+import akka.stream.scaladsl.{Flow, JsonFraming, Source, StreamConverters}
 import akka.util.ByteString
-import no.simula.umod.redditdatasetstreampipeline.model.{Comment, ModelEntity, Submission, ToCsv}
-import no.simula.umod.redditdatasetstreampipeline.model.ModelEntity.ModelEntity
 import no.simula.umod.redditdatasetstreampipeline.model.JsonFormats._
+import no.simula.umod.redditdatasetstreampipeline.model.ModelEntity.ModelEntity
+import no.simula.umod.redditdatasetstreampipeline.model.{Comment, ModelEntity, Submission, ToCsv}
 import org.apache.commons.compress.compressors.{CompressorException, CompressorStreamFactory}
 import spray.json._
 
@@ -23,10 +23,7 @@ object Flows {
    */
   def ndJsonToObj(entity: ModelEntity) : Flow[ByteString, ToCsv, NotUsed] = {
     val f = Flow[ByteString]
-      .via(Framing.delimiter( //chunk the inputs up into actual lines of text
-        ByteString("\n"),
-        maximumFrameLength = Int.MaxValue,
-        allowTruncation = true))
+      .via(JsonFraming.objectScanner(Int.MaxValue))
 
     entity match {
       case ModelEntity.SubmissionEntity => f.map(_.utf8String.parseJson.convertTo[Submission])
@@ -39,13 +36,13 @@ object Flows {
    * Takes NdJson ByteStrings and converts them to Submission objects
    */
   val ndJsonToSubmission: Flow[ByteString, ToCsv, NotUsed] = Flow[ByteString]
-    .via(Framing.delimiter( //chunk the inputs up into actual lines of text
-      ByteString("\n"),
-      maximumFrameLength = Int.MaxValue,
-      allowTruncation = true))
+    // .via(Framing.delimiter( //chunk the inputs up into actual lines of text
+    //   ByteString("\n"),
+    //     maximumFrameLength = Int.MaxValue,
+    //     allowTruncation = true))
     // Possible but costlier alternative to new lines would be
     // To scan the stream for json objects
-    // .via(JsonFraming.objectScanner(Int.MaxValue))'
+    .via(JsonFraming.objectScanner(Int.MaxValue))
     .map(_.utf8String.parseJson.convertTo[Submission](submissionFormat)) // Create json objects
 
 
