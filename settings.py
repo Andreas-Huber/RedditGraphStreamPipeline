0cd6ec6803ac84a010845d139f898d8a37ddf38d
Andreas Huber <andreas.huber@infinite-coding.com>
2021-03-13 14:05:52 +0100

    Edge vertex list plots 10k

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 
+# Import
 EXPERIMENTS_DIR = "../experiments"
 FILTER_LISTS_DIR = "../filterlists"
+GRAPHS_DIR = "../gdrive/Graphs"
+
+#Export
 FIGURES_DIR = "../MasterThesis/figures"
 TABLES_DIR = "../MasterThesis/tables"
